eda24ce2964f5d0a2352860d924a5a2e4ceaf69f
vishnu <vishnusubramanium@gmail.com>
2022-08-15 16:39:22 +0530

    first commit demo

---
+++ Final-Project-Placement-management-System-main/README.md
@@ -0,0 +1,2 @@
+# Final-Project-Placement-management-System
+In this Placement management system we can add student details,college details,placement details and we can modify the data given and can be saved
---
+++ Final-Project-Placement-management-System-main/src/main/java/com/cg/placementmanagement/Placement/PlacementManagementApplication.java
@@ -0,0 +1,16 @@
+package com.cg.placementmanagement.Placement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class PlacementManagementApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacementManagementApplication.class, args);
+	}
+	
+	
+
+}
